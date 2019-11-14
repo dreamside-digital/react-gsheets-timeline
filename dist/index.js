00ffec202a3fc0e549c9fb9c478f13b6bb507ba1
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-11-14 13:18:37 -0500

    update build

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Timeline = undefined;
 
 var _Timeline = require('./components/Timeline');
 
@@ -11,4 +10,4 @@ var _Timeline2 = _interopRequireDefault(_Timeline);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.Timeline = _Timeline2.default;
+exports.default = _Timeline2.default;
